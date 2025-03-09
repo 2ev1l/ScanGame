c146b3feb211a0fabf8550df12423e6ec66db704
2ev1l <xxx2ev1l@gmail.com>
2025-03-10 00:57:38 +0300

    Added mini game 9

--- Assets/Scripts/Game/UI/Overlay/PerspecitveScale.cs
+++ Assets/Scripts/Game/UI/Overlay/PerspecitveScale.cs
@@ -9,6 +9,8 @@ namespace Game.UI.Overlay
     {
         #region fields & properties
         [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private bool originalY = true;
+        [SerializeField][DrawIf(nameof(originalY), false)] private RectTransform relatedYTransform;
         [SerializeField] private Vector2 minMaxY = new(0, 100);
         [SerializeField] private Vector2 minMaxScale = new(1, 1.1f);
         #endregion fields & properties
@@ -17,7 +19,8 @@ namespace Game.UI.Overlay
         [SerializedMethod]
         public void ApplyScale()
         {
-            float y = rectTransform.anchoredPosition3D.y;
+            RectTransform transformY = originalY ? rectTransform : relatedYTransform;
+            float y = transformY.anchoredPosition3D.y;
             float lerp = Mathf.InverseLerp(minMaxY.x, minMaxY.y, y);
             Vector3 scale = Vector3.one * Mathf.Lerp(minMaxScale.x, minMaxScale.y, lerp);
             rectTransform.localScale = scale;
